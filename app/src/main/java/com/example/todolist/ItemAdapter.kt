b90e0882a798d12568ceb225590ea5e0be375cee
regina <usmanova.re@yandex.ru>
2024-02-28 18:41:47 +0500

    Основные моменты с адаптером
    ➤Создается класс  ItemAdapter, который наследуется от RecyclerView.Adapter<ItemAdapter.MyViewHolder>
    ➤Внутри класса ItemAdapter объявляется внутренний класс MyViewHolder, наследуемый от RecyclerView.ViewHolder
    ➤В MyViewHolder объявляется переменная textViewItemTaskList типа TextView, которая инициализируется с помощью метода findViewById
    ➤Переопределяется метод onCreateViewHolder, который создает новый объект MyViewHolder, заполняя его данными из макета item_task_list_activity
    ➤Переопределяется метод onBindViewHolder, который заполняет textViewItemTaskList в MyViewHolder текстом из списка dataList по позиции position
    ➤Переопределяется метод getItemCount, который возвращает размер списка dataList
    
        В этом коде `MyAdapter` - это адаптер для `RecyclerView`, который принимает список строк в качестве данных. `MyViewHolder` содержит ссылку на `TextView`, который отображает данные. В методе `onBindViewHolder` устанавливается текст для каждого элемента списка.

--- app/src/main/java/com/example/todolist/ItemAdapter.kt
+++ app/src/main/java/com/example/todolist/ItemAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.todolist
 
-class ItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(private val dataList: List<String>) : RecyclerView.Adapter<ItemAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewItemTaskList: TextView = itemView.findViewById(R.id.textViewItemTaskList)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_task_list_activity, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textViewItemTaskList.text = dataList[position]
+    }
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
 }
